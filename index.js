fb9d48ff1db3ef1ab6230363ce476dc5a75e61f0
happy4518 <baricksatyajeet@gmail.com>
2024-07-16 00:34:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//variable declaration
+
+var x= 5;
+let y= 6;
+console.log(x);
+console.log(y);
+
+// const declaration 
+const z= true;
+console.log(z);
+
+// data types
+
+let num=3;
+console.log(typeof(num));
+let str = "abc";
+console.log(typeof(str));
+let b = true;
+console.log(typeof(b));
+ let obj={
+    id:10,
+    name:"happy"
+ };
+ console.log(typeof(obj));
+ let arr = [1,2,3,4]
+ console.log(typeof(arr));
+
+ // reassigning variables
+
+ let name= "happy";
+ console.log(name);
+// let name = "shym"
+
+const g= 5;
+console.log(g);
+// const g= 6; 
+// console.log(g);
+
+
+
+
+ 
+ 
